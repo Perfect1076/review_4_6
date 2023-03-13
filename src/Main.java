16034883f7391172098cf9db044274c4165bf8b8
Perfect1076 <hadespwns@gmail.com>
2023-03-13 01:09:53 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> test = new ArrayList<String>();
+
+
+
+        Comparator<String> y = new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                if (a.length() < b.length()) {
+                    return -1;
+                } else if (a.length() > b.length()) {
+                    return 1;
+
+                } else {
+                    return 0;
+                }
+            }
+        };
+
+        test.add("Hello");
+
+        test.add("1");
+        test.add("cool");
+
+        test.add("coolioooooooo!");
+
+
+     System.out.println(   maximum(test, y));
+    }
+
+    public static String maximum(ArrayList<String> x, Comparator<String> y) {
+        String temp = "";
+        for(int i = 0; i< x.size(); i++){
+            if(y.compare(temp, x.get(i)) < 0){
+                temp = x.get(i);
+            }
+        }
+
+        return temp;
+
+
+    }
+
+
+
+}
